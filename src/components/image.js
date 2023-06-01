b4a174e6d08e47b81886bcf6b30c11391e8b5223
Eray Yıldız <eray1yildiz@hotmail.com>
2023-06-01 16:00:00 +0300

    tamamlandı

---
+++ src/components/image.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Image = props => {
+  const { dataUrl } = props;
+  return (
+    <div>
+      <img src={dataUrl} />
+    </div>
+  );
+};
+
+export default Image;
